942d9bc6093cd1bb3cecf78ec45aaa8f8b0965d7
TimmyChan99 <felemenoun@gmail.com>
2022-05-23 15:09:58 +0100

    Create splash screen page and set up the route

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,3 +1,5 @@
 class UsersController < ApplicationController
   def home; end
+  def splash; end
+    
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,17 @@
 Rails.application.routes.draw do
   devise_for :users
-  root "users#home"
-  get 'home', to: 'users#home'
+
+  devise_scope :user do
+    authenticated :user do
+      root 'users#home', as: :authenticated_root
+    end
+  
+    unauthenticated do
+      root 'users#splash', as: :unauthenticated_root
+    end
+  end
+
+  # root "users#home"
+  #get 'home', to: 'users#home'
   resources :users
 end
