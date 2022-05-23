9769ef42b5448d4f089c024be4c2b8c7fcd9304c
TimmyChan99 <felemenoun@gmail.com>
2022-05-23 12:32:03 +0100

    Setup devise views

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_05_23_103214) do
+ActiveRecord::Schema[7.0].define(version: 2022_05_23_111859) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "entities", force: :cascade do |t|
     t.string "name"
-    t.decimal "amount"
+    t.decimal "amount", default: "0.0"
     t.bigint "author_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
@@ -38,6 +38,13 @@ ActiveRecord::Schema[7.0].define(version: 2022_05_23_103214) do
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
   add_foreign_key "entities", "users", column: "author_id"
