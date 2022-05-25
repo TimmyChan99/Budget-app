6610e5a30f0cbeec1ea53a24381a8f0711a6d368
TimmyChan99 <felemenoun@gmail.com>
2022-05-25 14:46:06 +0100

    Fix linter errors

--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -2,12 +2,12 @@ require 'rails_helper'
 
 RSpec.describe Group, type: :model do
   subject do
-		user = User.create(id: 1, name: 'user', email: 'user@dev.co', password: '123456')
+    user = User.create(id: 1, name: 'user', email: 'user@dev.co', password: '123456')
     Group.new(name: 'Books', icon: 'url', author_id: user.id)
   end
   before { subject.save }
 
-	it 'Should be valid ' do
+  it 'Should be valid ' do
     expect(subject).to be_valid
   end
 
@@ -20,5 +20,4 @@ RSpec.describe Group, type: :model do
     subject.icon = nil
     expect(subject).to_not be_valid
   end
-
 end
